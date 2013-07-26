90850504e4639759912d0322385f010bf6d1b5aa
glorieux <lorieux.g@gmail.com>
2013-07-26 16:12:51 +0200

    Add mininal rails generator
    Add a rails generator that create a config file from the client_id and
    client_name inputed

---
+++ lib/generators/mangopay/install_generator.rb
@@ -0,0 +1,29 @@
+require 'mangopay'
+
+module MangoPay
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      namespace 'mangopay'
+      source_root File.expand_path('./templates', __FILE__)
+      argument :client_id, type: :string,
+        desc: 'The id you want to use to query the MangoPay API (must match with the regex ^[a-z0-9_-]{4,20}$)'
+      argument :client_name, type: :string, desc: "Full name of you're organization"
+      class_option :preproduction, type: :boolean, default: true
+
+      desc 'Installs all the basic configuration of the mangopay gem'
+      def install_mangopay
+        template 'mangopay.rb', 'config/initializers/mangopay.rb'
+      end
+
+      protected
+
+      def client_passphrase
+        client = MangoPay::Client.create({
+          ClientID: client_id,
+          Name: client_name
+        })
+        client['Passphrase']
+      end
+    end
+  end
+end
--- mangopay.gemspec
+++ mangopay.gemspec
@@ -24,6 +24,7 @@ Gem::Specification.new do |s|
   s.add_development_dependency('rspec', '~> 2.14.1')
   s.add_development_dependency('capybara', '~> 2.1.0')
   s.add_development_dependency('capybara-webkit', '~> 1.0.0')
+  s.add_development_dependency('rails', '~> 4.0.0')
 
   s.files         = `git ls-files`.split("\n")
   s.test_files    = `git ls-files -- spec/*`.split("\n")
