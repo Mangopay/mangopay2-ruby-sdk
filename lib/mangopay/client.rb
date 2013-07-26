1d139749ea96f42cf8647d69ca428bb7c5350774
glorieux <lorieux.g@gmail.com>
2013-07-26 17:29:09 +0200

    Now use an outside method to create the client account

--- lib/mangopay/client.rb
+++ lib/mangopay/client.rb
@@ -1,5 +1,17 @@
 module MangoPay
   class Client < Resource
-    include MangoPay::HTTPCalls::Create
+    def self.create(params)
+      uri = URI(MangoPay.configuration.root_url + '/api/clients/')
+      res = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+        puts uri.request_uri
+        request = Net::HTTP::Post.new(uri.request_uri, {
+          'user_agent' => "MangoPay V1 RubyBindings/#{MangoPay::VERSION}",
+          'Content-Type' => 'application/json'
+        })
+        request.body = MangoPay::JSON.dump(params)
+        http.request request
+      end
+      MangoPay::JSON.load(res.body)
+    end
   end
 end
