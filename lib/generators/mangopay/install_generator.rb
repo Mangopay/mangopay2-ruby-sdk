f05d1f70820e0ccde6030b3a836a1aa72bb6ca0e
glorieux <lorieux.g@gmail.com>
2013-07-26 16:38:44 +0200

    Update preproduction description. chenge the source_root

--- lib/generators/mangopay/install_generator.rb
+++ lib/generators/mangopay/install_generator.rb
@@ -4,11 +4,11 @@ require 'mangopay'
 module Mangopay
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path('../templates', __FILE__)
+      source_root File.expand_path('../../templates', __FILE__)
       argument :client_id, type: :string,
         desc: 'The id you want to use to query the MangoPay API (must match with the regex ^[a-z0-9_-]{4,20}$)'
       argument :client_name, type: :string, desc: "Full name of you're organization"
-      class_option :preproduction, type: :boolean, default: true
+      class_option :preproduction, type: :boolean, default: true, desc: 'Whether or not use the preproduction environment'
 
       desc 'Installs all the basic configuration of the mangopay gem'
       def setup
