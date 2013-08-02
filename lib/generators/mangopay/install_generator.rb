fa07b42a6e176a5b290ab86a34f1b16635017797
glorieux <lorieux.g@gmail.com>
2013-08-02 17:27:22 +0200

    Better generator.
    The generator now checks the client_id validity before it gets submited.
    It also alert the user if the client_id is already taken.
    Refactoring of the methods to be simpler.

--- lib/generators/mangopay/install_generator.rb
+++ lib/generators/mangopay/install_generator.rb
@@ -12,32 +12,46 @@ module Mangopay
 
       desc 'Installs all the basic configuration of the mangopay gem'
       def setup
-        if check_client_id
+        begin
+          client = client_id_valid?
           remove_file 'config/initializers/mangopay.rb'
           @client_id = client_id
-          @client_passphrase = client_passphrase
+          @client_passphrase = client['Passphrase']
           template 'mangopay.rb', 'config/initializers/mangopay.rb'
-        else
-          puts "client_id must match the regexp ^[a-z0-9_-]{4,20}$"
+        rescue => e
+          puts e.message
         end
       end
 
       protected
 
-      def check_client_id
-        (/^[a-z0-9_-]{4,20}$/ =~ client_id).nil? ? false : true
+      def client_id_valid?
+        check_client_id_validity
+        check_client_id_availablility
       end
 
-      def client_passphrase
+      def check_client_id_validity
+        if (/^[a-z0-9_-]{4,20}$/ =~ client_id).nil?
+          raise "The client_id must match the regexp ^[a-z0-9_-]{4,20}$"
+        end
+      end
+
+      def check_client_id_availablility
+        client = create_client
+        if client['Type'] == 'ClientID_already_exist'
+          raise client['Message']
+        end
+        client
+      end
+
+      def create_client
         MangoPay.configure do |c|
           c.preproduction = options[:preproduction]
         end
-        client = MangoPay::Client.create({
+        MangoPay::Client.create({
           ClientID: client_id,
           Name: client_name
         })
-        # TODO: Check for existing id
-        client['Passphrase']
       end
     end
   end
