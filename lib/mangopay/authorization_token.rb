1027980f108a444875b7a8de13735f5e8ad1ccdb
Sergiusz Woźnicki <sergiusz.woznicki@mosaic-lab.com>
2014-06-30 11:07:16 +0200

    Get token call: URL slightly changed
    '/api/oauth/token' => '/v2/oauth/token'

--- lib/mangopay/authorization_token.rb
+++ lib/mangopay/authorization_token.rb
@@ -15,7 +15,7 @@ module MangoPay
         def get_token
           token = storage.get
           if token.nil? || token['timestamp'].nil? || token['timestamp'] <= Time.now
-            token = MangoPay.request(:post, '/api/oauth/token', {}, {}, {}, Proc.new do |req|
+            token = MangoPay.request(:post, '/v2/oauth/token', {}, {}, {}, Proc.new do |req|
               cfg = MangoPay.configuration
               req.basic_auth cfg.client_id, cfg.client_passphrase
               req.body = 'grant_type=client_credentials'
