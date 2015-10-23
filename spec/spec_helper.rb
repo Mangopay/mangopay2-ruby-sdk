cfffaf040a6708d31761faee4fb10058344b4e66
Sergiusz Woźnicki <sergiusz.woznicki@mosaic-lab.com>
2015-10-23 12:02:10 +0200

    bugfix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ def reset_mangopay_configuration
   MangoPay.configure do |c|
     c.preproduction = true
     c.client_id = 'sdk-unit-tests'
+    c.client_passphrase = 'cqFfFrWfCcb7UadHNxx2C9Lo6Djw8ZduLi7J9USTmu8bhxxpju'
     c.temp_dir = File.expand_path('../tmp', __FILE__)
     require 'fileutils'
     FileUtils.mkdir_p(c.temp_dir) unless File.directory?(c.temp_dir)
