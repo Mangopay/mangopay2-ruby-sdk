e4bd7c0eb6847de94bff40d69e91ea10908b9880
Sergiusz Woźnicki <sergiusz.woznicki@mosaic-lab.com>
2016-03-18 11:46:32 +0100

    Add Client methods: fetch, update, upload_logo
    See https://docs.mangopay.com/api-references/client-details/

--- lib/mangopay/client.rb
+++ lib/mangopay/client.rb
@@ -1,11 +1,41 @@
+require 'base64'
+
 module MangoPay
   class Client < Resource
 
-    def self.create(params)
-      MangoPay.request(:post, '/api/clients/', params, {}, {
-        'user_agent' => "MangoPay V2 RubyBindings/#{VERSION}",
-        'Content-Type' => 'application/json'
-      })
+    class << self
+
+      def create(params)
+        MangoPay.request(:post, '/api/clients/', params, {}, {
+          'user_agent' => "MangoPay V2 RubyBindings/#{VERSION}",
+          'Content-Type' => 'application/json'
+        })
+      end
+
+      # see https://docs.mangopay.com/api-references/client-details/
+      def fetch()
+        MangoPay.request(:get, url())
+      end
+
+      # see https://docs.mangopay.com/api-references/client-details/
+      def update(params)
+        MangoPay.request(:put, url(), params)
+      end
+
+      # see https://docs.mangopay.com/api-references/client-details/
+      def upload_logo(file_content_base64, file_path = nil)
+        if file_content_base64.nil? && !file_path.nil?
+          bts = File.open(file_path, 'rb') { |f| f.read }
+          file_content_base64 = Base64.encode64(bts)
+        end
+        # normally it returns 204 HTTP code on success
+        begin
+          MangoPay.request(:put, url() + '/logo', {'File' => file_content_base64})
+        rescue ResponseError => ex
+          raise ex unless ex.code == '204'
+        end
+      end
+
     end
   end
 end
--- spec/mangopay/client_spec.rb
+++ spec/mangopay/client_spec.rb
@@ -25,4 +25,50 @@ describe MangoPay::Client do
       }.to raise_error MangoPay::ResponseError
     end
   end
+
+  describe 'FETCH' do
+    it 'fetches the current client details' do
+      clnt = MangoPay::Client.fetch
+      expect(clnt['ClientId']).to eq(MangoPay.configuration.client_id)
+    end
+  end
+
+  describe 'UPDATE' do
+    it 'updates the current client details' do
+      clnt = MangoPay::Client.fetch
+      before = clnt['PrimaryThemeColour']
+      after = before == '#aaaaaa' ? '#bbbbbb' : '#aaaaaa' # change the color
+      clnt['PrimaryThemeColour'] = after
+
+      updated = MangoPay::Client.update(clnt)
+      expect(updated['ClientId']).to eq(MangoPay.configuration.client_id)
+      expect(updated['PrimaryThemeColour']).to eq(after)
+    end
+  end
+
+  describe 'UPLOAD LOGO' do
+    it 'accepts Base64 encoded file content' do
+      fnm = __FILE__.sub('.rb', '.png')
+      bts = File.open(fnm, 'rb') { |f| f.read }
+      b64 = Base64.encode64(bts)
+      ret = MangoPay::Client.upload_logo(b64)
+      expect(ret).to be_nil
+    end
+
+    it 'accepts file path' do
+      fnm = __FILE__.sub('.rb', '.png')
+      ret = MangoPay::Client.upload_logo(nil, fnm)
+      expect(ret).to be_nil
+    end
+
+    it 'fails when input string is not base64-encoded' do
+      file = 'any file content...'
+      expect { MangoPay::Client.upload_logo(file) }.to raise_error { |err|
+        expect(err).to be_a MangoPay::ResponseError
+        expect(err.code).to eq '400'
+        expect(err.type).to eq 'param_error'
+      }
+    end
+  end
+
 end
