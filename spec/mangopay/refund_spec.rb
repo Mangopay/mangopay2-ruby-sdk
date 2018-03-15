1f0c33dab737d44aa837a818d61e6396dd945165
Paul Hristea <paul.hristea@codegile.com>
2018-03-15 17:42:13 +0200

    Using another Repudiation ID for a test

--- spec/mangopay/refund_spec.rb
+++ spec/mangopay/refund_spec.rb
@@ -21,7 +21,7 @@ describe MangoPay::Refund do
 
   describe 'FETCH for Repudiation' do
     it "fetches a repudiation's refunds" do
-      refunds = MangoPay::Refund.of_repudiation('45026109')
+      refunds = MangoPay::Refund.of_repudiation('45135438')
       expect(refunds).to be_an(Array)
     end
   end
