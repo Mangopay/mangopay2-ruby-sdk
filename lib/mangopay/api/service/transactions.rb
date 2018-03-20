194795e766dd7f6f344d47fe5d33d456d06005dc
Paul Hristea <paul.hristea@codegile.com>
2018-03-20 20:31:53 +0200

    Implemented Get Transactions for a Mandate endpoint

--- lib/mangopay/api/service/transactions.rb
+++ lib/mangopay/api/service/transactions.rb
@@ -157,6 +157,26 @@ module MangoApi
         parse_results results
       end
 
+      # Retrieves pages of transaction entities belonging to a certain
+      # mandate. Allows configuration of paging and sorting parameters
+      # by yielding a filtering object to a provided block. When no filters
+      # are specified, will retrieve the first page of 10 newest results.
+      #
+      # Allowed +FilterRequest+ params:
+      # * page
+      # * per_page
+      # * sort_field and sort_direction
+      #
+      # @param +id+ [String] ID of the mandate whose transactions to retrieve
+      # @return [Array] the requested Transaction entity objects
+      def of_mandate(id)
+        uri = provide_uri(:get_mandates_transactions, id)
+        filter_request = nil
+        yield filter_request = FilterRequest.new if block_given?
+        results = HttpClient.get(uri, filter_request)
+        parse_results results
+      end
+
       private
 
       # Parses an array of JSON-originating hashes into the corresponding
