95eff8acc70b276ae71566f749b6d96ead431def
SoloJr <mircea.solo95@yahoo.com>
2020-09-23 21:56:37 +0300

    enabled validation endpoint test

--- spec/mangopay/client_spec.rb
+++ spec/mangopay/client_spec.rb
@@ -125,18 +125,18 @@ describe MangoPay::Client do
       #expect((trns.map {|m| m['CreditedWalletId']}).uniq).to eq(['CREDIT_EUR'])
     end
   end
-  ##
-  #describe 'validate' do
-  #  it 'validates card' do
-  #     client = MangoPay::Client.fetch
-  #    completed = new_card_registration_completed
-  #    card_id = completed['CardId']
-  #    client_id = client['ClientId']
-  #
-  #    card = MangoPay::Client.validate(client_id, card_id)
-  #  expect(client).not_to be_nil
-  #     expect(card).not_to be_nil
-  #  end
-  #end
+
+  describe 'validate' do
+    it 'validates card' do
+      client = MangoPay::Client.fetch
+      completed = new_card_registration_completed
+      card_id = completed['CardId']
+      client_id = client['ClientId']
+
+      card = MangoPay::Client.validate(client_id, card_id)
+      expect(client).not_to be_nil
+      expect(card).not_to be_nil
+    end
+  end
 
 end
