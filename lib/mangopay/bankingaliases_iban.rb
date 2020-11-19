a5ac1404f72c92a360f944fd6a0b55af0433d31e
AndriiNyzhnyk <a.nyzhnyk@elopage.com>
2020-11-19 19:06:14 +0200

    Send headers to bank_alias

--- lib/mangopay/bankingaliases_iban.rb
+++ lib/mangopay/bankingaliases_iban.rb
@@ -3,9 +3,9 @@ module MangoPay
   # See parent class MangoPay::BankingAliases
   class BankingAliasesIBAN < BankingAliases
 
-    def self.create(params, wallet_id)
+    def self.create(params, wallet_id, headers_or_idempotency_key = nil)
       url = "#{MangoPay.api_path}/wallets/#{wallet_id}/bankingaliases/iban"
-      MangoPay.request(:post, url, params, {})
+      MangoPay.request(:post, url, params, {}, headers_or_idempotency_key)
     end
 
     def self.url(id = nil)
