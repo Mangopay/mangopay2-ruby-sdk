33334f14be05cc2a9f9c0abe64a7bc877246e8e8
Mircea Solovastru <mircea.solo95@yahoo.com>
2021-03-25 11:08:44 +0200

    removed 3ds

--- lib/mangopay/model/entity/pay_in/card_web_pay_in.rb
+++ lib/mangopay/model/entity/pay_in/card_web_pay_in.rb
@@ -36,11 +36,5 @@ module MangoModel
 
     # [Shipping] Shipping information
     attr_accessor :shipping
-
-    # [String] Requested3DSVersion
-    attr_accessor :requested_3ds_version
-
-    # [String] Applied3DSVersion
-    attr_accessor :applied_3ds_version
   end
 end
