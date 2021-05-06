73e16d2eb44eb345f699c73114a80ad72a736e4e
AndriiNyzhnyk <a.nyzhnyk@elopage.com>
2021-05-06 10:40:16 +0300

    Fix specs

--- spec/mangopay/ubo_spec.rb
+++ spec/mangopay/ubo_spec.rb
@@ -5,7 +5,7 @@ describe MangoPay::Ubo do
   describe 'FETCH' do
     it 'can fetch a Ubo' do
       legal_user = new_legal_user
-      ubo_declaration = MangoPay::UboDeclaration.create(legal_user['Id'])
+      ubo_declaration = MangoPay::UboDeclaration.create(legal_user['Id'], nil)
       ubo = new_ubo(legal_user, ubo_declaration)
       result = MangoPay::Ubo.fetch(legal_user['Id'], ubo_declaration['Id'], ubo['Id'])
       expect(result).not_to be_nil
@@ -16,7 +16,7 @@ describe MangoPay::Ubo do
   describe 'CREATE' do
     it 'can create a new Ubo' do
       legal_user = new_legal_user
-      ubo_declaration = MangoPay::UboDeclaration.create(legal_user['Id'])
+      ubo_declaration = MangoPay::UboDeclaration.create(legal_user['Id'], nil)
       result = new_ubo(legal_user, ubo_declaration)
       expect(result).not_to be_nil
     end
@@ -25,7 +25,7 @@ describe MangoPay::Ubo do
   describe 'UPDATE' do
     it 'can update a Ubo' do
       legal_user = new_legal_user
-      ubo_declaration = MangoPay::UboDeclaration.create(legal_user['Id'])
+      ubo_declaration = MangoPay::UboDeclaration.create(legal_user['Id'], nil)
       ubo = new_ubo(legal_user, ubo_declaration)
       ubo['FirstName'] = 'Santa'
       ubo['LastName'] = 'Clause'
