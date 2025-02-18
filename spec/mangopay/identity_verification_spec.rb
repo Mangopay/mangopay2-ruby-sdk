37aeff1c6a4b2eec9bc2cd076bae20c6a4c296a2
Iulian Masar <iulian.masar@codegile.com>
2025-02-18 14:49:24 +0200

    added support for fetching an identity verification

--- spec/mangopay/identity_verification_spec.rb
+++ spec/mangopay/identity_verification_spec.rb
@@ -1,8 +1,6 @@
 describe MangoPay::IdentityVerification do
   include_context 'users'
 
-  let(:identity_verification) { nil }
-
   describe 'CREATE' do
     it 'creates a new identity verification' do
       created = create_new_identity_verification
@@ -13,18 +11,27 @@ describe MangoPay::IdentityVerification do
     end
   end
 
+  describe 'GET' do
+    it 'creates fetches existing identity verification' do
+      created = create_new_identity_verification
+      fetched = MangoPay::IdentityVerification.get(created['Id'])
+
+      expect(fetched).not_to be_nil
+      expect(created['HostedUrl']).to eq(fetched['HostedUrl'])
+      expect(created['ReturnUrl']).to eq(fetched['ReturnUrl'])
+      expect(created['Status']).to eq(fetched['Status'])
+    end
+  end
+
   def create_new_identity_verification
     user = new_natural_user
-    if identity_verification == nil
-      identity_verification = MangoPay::IdentityVerification.create(
-        {
-          ReturnUrl: 'http://example.com',
-          Tag: 'created by the Ruby SDK'
-        },
-        user['Id']
-      )
-    end
-    return identity_verification
+    return MangoPay::IdentityVerification.create(
+      {
+        ReturnUrl: 'http://example.com',
+        Tag: 'created by the Ruby SDK'
+      },
+      user['Id']
+    )
   end
 
 end
